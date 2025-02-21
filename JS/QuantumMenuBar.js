220d63a3cf1267c6fc61c46073c029b4da27ba83
zambranorenn <rennyxd2000@gmail.com>
2025-02-21 01:21:27 -0400

    Quantum MenuBar (incompleto)

--- JS/QuantumMenuBar.js
+++ JS/QuantumMenuBar.js
@@ -1,34 +1,52 @@
-class MenuBar extends HTMLElement {
-    constructor() {
+export class QuantumMenuBar extends Quantum {
+    constructor(props) {
         super();
+        this.name = "QuantumMenuBar";
+        this.props = props;
+        if (props?.id) this.id = props.id;
         this.attachShadow({ mode: 'open' });
-        this.shadowRoot.innerHTML = `
-            <style>
-                @import url('menu-bar.css');
-            </style>
-            <nav class="menu"></nav>
+        this.built = () => {};
+    }
+
+    #getTemplate() {
+        return `
+        <nav class="menu">
+            <slot></slot> <!-- Aquí se insertarán los elementos hijos -->
+        </nav>
         `;
     }
 
-    connectedCallback() {
+    async #getCss() {
+        return await quantum.getCssFile("QuantumMenuBar");
+    }
+
+    async #render(css) {
+        this.template = document.createElement('template');
+        this.template.innerHTML = this.#getTemplate();
+        const sheet = new CSSStyleSheet();
+        sheet.replaceSync(css);
+        this.shadowRoot.adoptedStyleSheets = [sheet];
+        const tpc = this.template.content.cloneNode(true);
+        this.mainElement = tpc.firstElementChild;
+        this.shadowRoot.appendChild(this.mainElement);
         this.renderMenu();
     }
 
     renderMenu() {
         const menuContainer = this.shadowRoot.querySelector('.menu');
         const menuItems = Array.from(this.children);
-    
+
         menuItems.forEach(menuItem => {
             const title = menuItem.getAttribute('title');
             const itemElement = document.createElement('div');
             itemElement.classList.add('menu-item');
             itemElement.textContent = title;
-    
+
             const submenuItems = Array.from(menuItem.children);
             if (submenuItems.length > 0) {
                 const submenuElement = document.createElement('div');
                 submenuElement.classList.add('submenu');
-    
+
                 submenuItems.forEach(submenuItem => {
                     const subTitle = submenuItem.getAttribute('title');
                     const subItemElement = document.createElement('div');
@@ -36,7 +54,7 @@ class MenuBar extends HTMLElement {
                     subItemElement.textContent = subTitle;
                     submenuElement.appendChild(subItemElement);
                 });
-    
+
                 itemElement.appendChild(submenuElement);
                 itemElement.addEventListener('click', (event) => {
                     event.stopPropagation();
@@ -49,15 +67,47 @@ class MenuBar extends HTMLElement {
                     submenuElement.style.display = submenuElement.style.display === 'block' ? 'none' : 'block';
                 });
             }
-    
+
             menuContainer.appendChild(itemElement);
         });
     }
-}    
 
-class MenuItem extends HTMLElement {}
-class SubmenuItem extends HTMLElement {}
+    async connectedCallback() {
+        await this.#render(await this.#getCss());
+        this.built();
+    }
+}
 
-customElements.define('menu-bar', MenuBar);
-customElements.define('menu-item', MenuItem);
-customElements.define('submenu-item', SubmenuItem);
+export class QuantumMenuItem extends Quantum {
+    constructor(props) {
+        super();
+        this.name = "QuantumMenuItem";
+        this.props = props;
+        if (props?.id) this.id = props.id;
+        this.attachShadow({ mode: 'open' });
+        this.built = () => {};
+    }
+
+    async connectedCallback() {
+        this.built();
+    }
+}
+
+export class QuantumSubmenuItem extends Quantum {
+    constructor(props) {
+        super();
+        this.name = "QuantumSubmenuItem";
+        this.props = props;
+        if (props?.id) this.id = props.id;
+        this.attachShadow({ mode: 'open' });
+        this.built = () => {};
+    }
+
+    async connectedCallback() {
+        this.built();
+    }
+}
+
+customElements.define('quantum-menu-bar', QuantumMenuBar);
+customElements.define('quantum-menu-item', QuantumMenuItem);
+customElements.define('quantum-submenu-item', QuantumSubmenuItem);
