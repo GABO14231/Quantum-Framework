1295c6f16fbe3ae90b3ea8855280e2a94595c3c7
JeJaMel <jjmelendezb15@gmail.com>
2025-03-07 00:19:26 -0400

    feat: Add modal to wait

--- JS/QuantumWait.js
+++ JS/QuantumWait.js
@@ -12,15 +12,20 @@ export const QuantumWait = class extends Quantum {
     #getTemplate() {
         const template = document.createElement('template');
         template.innerHTML = `
-        <div>
+        <div class="icon-container" id="normal-icon-container">
             <img src="" class="QuantumWaitIcon" style="width: 50px; height: 50px;" />
+        </div>
+        <div id="overlay" class="overlay hidden">
+            <div class="icon-container">
+                <img src="" class="QuantumWaitIcon" />
+            </div>
         </div>
         `;
         return template;
     }
 
     static get observedAttributes() {
-        return ['icon-name', 'rotate', 'size'];
+        return ['icon-name', 'rotate', 'size', 'modal'];
     }
 
     attributeChangedCallback(name, oldValue, newValue) {
@@ -34,6 +39,9 @@ export const QuantumWait = class extends Quantum {
                     break;
                 case 'size':
                     this.updateSize();
+                    break;
+                case 'modal':
+                    this.updateModal();
                     break;
             }
         }
@@ -68,37 +76,59 @@ export const QuantumWait = class extends Quantum {
     async updateIcon() {
         const iconName = this.getAttribute('icon-name');
         const svgURL = await this.getSVG(iconName); // Get the SVG URL
-        const imgElement = this.shadowRoot.querySelector('.QuantumWaitIcon');
+        const imgElement = this.shadowRoot.querySelector('#normal-icon-container .QuantumWaitIcon');
+        const imgElementOverlay = this.shadowRoot.querySelector('#overlay .icon-container .QuantumWaitIcon');
 
         if (svgURL) {
             imgElement.src = svgURL; // Set the src to the URL
+            imgElementOverlay.src = svgURL
         } else {
             imgElement.src = '';
+            imgElementOverlay.src = ''
         }
     }
 
     updateRotation() {
-        const imgElement = this.shadowRoot.querySelector('.QuantumWaitIcon');
+        const imgElement = this.shadowRoot.querySelector('#normal-icon-container .QuantumWaitIcon');
+        const imgElementOverlay = this.shadowRoot.querySelector('#overlay .icon-container .QuantumWaitIcon');
         const rotateValue = this.getAttribute('rotate');
         const rotateSpeed = rotateValue ? rotateValue : '2s';
         if (this.hasAttribute('rotate')) {
             imgElement.style.animation = `rotate ${rotateSpeed} linear infinite`;
+            imgElementOverlay.style.animation = `rotate ${rotateSpeed} linear infinite`;
         } else {
             imgElement.style.animation = '';
+            imgElementOverlay.style.animation = '';
         }
     }
 
     updateSize() {
         const size = this.getAttribute('size') || '50px';
-        const imgElement = this.shadowRoot.querySelector('.QuantumWaitIcon');
+        const imgElement = this.shadowRoot.querySelector('#normal-icon-container .QuantumWaitIcon');
+        const imgElementOverlay = this.shadowRoot.querySelector('#overlay .icon-container .QuantumWaitIcon');
         imgElement.style.width = size;
         imgElement.style.height = size;
+        imgElementOverlay.style.width = size;
+        imgElementOverlay.style.height = size;
+    }
+
+    updateModal() {
+        const overlay = this.shadowRoot.getElementById('overlay');
+        const normalIconContainer = this.shadowRoot.getElementById('normal-icon-container');
+        if (this.hasAttribute('modal')) {
+            overlay.classList.remove('hidden');
+            normalIconContainer.classList.add('hidden'); // Hide the normal icon
+        } else {
+            overlay.classList.add('hidden');
+            normalIconContainer.classList.remove('hidden'); // Show the normal icon
+        }
     }
 
     updateAttributes() {
         this.updateIcon();
         this.updateRotation();
         this.updateSize();
+        this.updateModal();
     }
 }
 
